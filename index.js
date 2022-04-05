27dd60330a42c3c53683923335c7bc37249bc11f
Kenny Clare <kmitchellclare@gmail.com>
2022-04-05 14:16:08 -0700

    commit tree 45c37284766e0f60d2dad2508aa83d6b064d7034

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Follow along with the examples here
+/*
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+sayHelloTo("Guadalupe");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+console.log(sayHelloTo);
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+}
+say("Greetings", "Julio");
+
+function say(Goodbye, firstName) {
+    console.log`${Goodbye}, ${firstName}`;
+}
+say("Goodbye", "Julio");
+*/
+
+// function add(x, y) {
+//     return x + y;
+// }
+// console.log(add(80, 9000));
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+// console.log(say("Hello", "LIZ"))
+
+
+// function say(greeting, firstName) {
+//     console.log("I was called!");
+//     return `${greeting}, ${firstName}!`;
+// }
+// console.log(say("Howdy", "partner"));
+
+
+function say(favoriteFood) {
+    return `my favorite food is ${favoriteFood}.`;
+}
+
+console.log(say("Chicken"));
